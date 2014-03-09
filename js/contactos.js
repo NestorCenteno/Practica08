f98e0269db56174ac54fbc5ad8452e61e28cbf80
NestorCenteno <nestor.centeno@nuvemtecnologia.mx>
2014-03-08 20:08:43 -0600

    qwwe
    ewweqwe

--- js/contactos.js
+++ js/contactos.js
@@ -2,70 +2,63 @@ function listar(){
     function onSuccess(contacts) {
         $('#contactos .plastic').html('');
         for(i=0;i<=contacts.length;i++){
-            $('#contactos .plastic').append('<li><a href="tel:'+contacts[i].phoneNumbers[0]+'">'+contacts[i].name.formatted+'</a></li>');
+            $('#contactos .plastic').append('<li><a href="tel:'+contacts[i].phoneNumbers[0]+'">'+contacts[i].displayName+'</a></li>');
         }
     }
-
-function onError(contactError) {
-    alert('onError!');
-}
-
-// find all contacts with 'Bob' in any name field
-var options      = new ContactFindOptions();
-options.filter   = "c";
-options.multiple = true;
-var fields       = ["*"];
-navigator.contacts.find(fields, onSuccess, onError, options);
+    function onError(contactError) {
+        alert('onError!');
+    }
+    
+    // find all contacts with 'Bob' in any name field
+    var options      = new ContactFindOptions();
+    options.filter   = "c";
+    options.multiple = true;
+    var fields       = ["*"];
+    navigator.contacts.find(fields, onSuccess, onError, options);
 }
 
 function crear(nom,tel,mail){
-    alert('dentro de crear');
     document.addEventListener("deviceready", onDeviceReady, false);
 
     // device APIs are available
     //
     function onDeviceReady() {
         var myContact = navigator.contacts.create();
-        myContact.displayName=nom;
-        myContact.nickname=nom;
-        alert('var mycontact');
+        myContact.displayName = nom;
+        myContact.nickname = nom;
         
-        var contacto=new ContacName();
-        contacto.givenName=nom;
-        contacto.familyName="Prueba";
-        myContact.name=contacto;
-        alert('var contacto');
+        var contacto = new ContactName();
+        contacto.givenName = nom;
+        contacto.familyName = "Prueba";
+        myContact.name = contacto;
         
-        var telefonos=[];
-        telefonos[0]=new ContactField("home",tel,true);
-        telefonos[1]=new ContactField("mobile","1223323223",false);
-        myContact.phoneNumbers=telefonos;
-        alert('var telefonos');
+        var telefonos = [];
+        telefonos[0] = new ContactField("home",tel,true);
+        telefonos[1] = new ContactField("mobile","123-123-1234",false);
+        myContact.phoneNumbers = telefonos;
         
-        var correos=[];
-        correos[0]=new ContactField("home",mail,true);
-        myContact.emails=correos;
-        alert('correos');
+        var correos = [];
+        correos[0] = new ContactField("home",mail,true);
+        myContact.emails = correos;
         
         myContact.save(function(){
-            navigator.notification.alert("el contacto ha sido creado",null,"contacto","aceptar");
+            navigator.notification.alert("El contacto ha sido creado",null,"Contacto","Aceptar");
         },function(){
-            alert("no se pudo guardar el contacto");
+            alert('No se pudo Guardar el contacto');
         });
     }
-
 }
-
+    
 $(function(){
     $('#acSend').tap(function(){
-        var nom=$('#nc .rounded input').eq(0).val();
-        var mail=$('#nc .rounded input').eq(1).val();
-        var tel=$('#nc .rounded input').eq(2).val();
+        var nom = $('#nc .rounded input').eq(0).val();
+        var tel = $('#nc .rounded input').eq(2).val();
+        var mail = $('#nc .rounded input').eq(1).val();
+        
         crear(nom,tel,mail);
     });
     
-    $("#contactos .individual li").eq(0).tap(function(){
+    $('#contactos .individual li').eq(0).tap(function(){
         listar();
-        alert('lista creada');
     });
 });
